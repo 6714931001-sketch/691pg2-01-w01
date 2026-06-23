fd70a5311efb36f5f6f47a6201a410f770471bdf
6714931001-sketch <6714931001@rbru.ac.th>
2026-06-23 21:39:42 +0700

    Add files via upload

---
+++ W02_01.py
@@ -0,0 +1,11 @@
+def main():
+    a = 2
+    b = 3
+    c = a + b
+    print(f"Output of {a} + {b} is {c}")
+
+    for i in range(0, 10, 1):
+        print(i)
+if __name__ == "__main__":
+    main()
+    
